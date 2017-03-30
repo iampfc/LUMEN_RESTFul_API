0af36c7970cfc5f5f62f49bb33dfb8f7251cf555
wing <183862684@qq.com>
2017-03-30 21:58:34 +0800

    init

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->version();
+});
+
+$app->get('/hello', function () use ($app) {
+    return 'Hello World';
+});
+
+$app->get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    echo '$postId:' . $postId .'<br>';
+    echo '$commentId:' . urldecode($commentId) .'<br>';
+});
